50361d58e1f6f6cd547bdbd5f7b06dde6a7a34b1
sovsianykov <swebproger@gmail.com>
2021-01-16 01:48:40 +0200

    dezer

--- src/app.js
+++ src/app.js
@@ -1,10 +1,43 @@
 import {http} from './http'
-import  {ui} from "./ui";
 
 
-addEventListener('DOMContentLoaded', getPosts)
-function getPosts() {
-    http.get( 'http://localhost:3000/posts')
-        .then(data => ui.showPosts(data))
-        .catch(err =>console.log(err))
+
+addEventListener('DOMContentLoaded', getImage)
+
+
+function getImage() {
+
+    const place = document.querySelector('#playlists')
+
+    http.get( 'https://api.deezer.com/radio' )
+        .then( data => data.data.forEach(el => {
+console.log(el)
+                const td = document.createElement('div')
+                const img = document.createElement('img')
+                const title = document.createElement('h5')
+                const div = document.createElement('div')
+                title.innerHTML =el.title
+                // div.style.display = 'inline-block'
+                // td.style.margin = '5px'
+
+
+                img.src = el.picture
+                div.appendChild(title)
+                div.appendChild(img)
+
+
+                place.appendChild(div)
+            }
+
+        ) )
+
+
+
+
+
+
+
+
+      .catch(err =>console.log(err))
 }
+
